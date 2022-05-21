8bf72eeffb103aaa9a0a921d646f27a3db41c8c6
i-am-sam-codes <sammmceja@gmail.com>
2022-05-21 17:31:01 -0400

    centers title

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+// import styles from '../styles/Home.module.css'
 import { ConnectButton } from '@rainbow-me/rainbowkit';
+import styles from "../styles/styles.module.css";
 
 export default function Home() {
   return (
